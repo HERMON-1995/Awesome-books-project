f13414301f08fa03c518b76916eed5f51f06fd2e
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-13 22:45:38 +0300

    Update main.js file

--- JS/main.js
+++ JS/main.js
@@ -1,93 +1,96 @@
+/* eslint-disable */
 class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
-
 class UI {
-    static displayBooks() {
-        const books = Store.getBooks();
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
-        books.forEach((book) => UI.addBookToList(book));
-    }
+  static addBookToList(book) {
+    const list = document.getElementById('book-list');
+    const ul = document.createElement('ul');
 
-    static addBookToList(book) {
-        const list = document.getElementById("book-list");
-        const ul = document.createElement('ul');
-
-        ul.innerHTML = `
+    ul.innerHTML = `
+        <hr>
            <li>${book.title}</li>
            <li>${book.author}</li><br>
-           <button class="btn delete">Remove</button><hr>
+           <button onclick="Store.removeBook(${book.id})" class="btn delete">Remove</button>
         `;
 
-        list.appendChild(ul);
-    }
+    list.appendChild(ul);
+  }
 
-    static deleteBook(el) {
-        if(el.classList.contains('delete')) {
-           el.parentElement.remove();
-        }
+  static deleteBook(elem) {
+    if (elem.classList.contains('delete')) {
+      elem.parentElement.remove();
     }
+  }
 
-    static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-    }
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
 }
 
 // Store Class
 class Store {
-    static getBooks() {
-      let books;
-      if(localStorage.getItem('books') === null) {
-         books = [];
-      }
-      else {
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-      return books;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
+    return books;
+  }
 
-    static addBook(book) {
-      const books = Store.getBooks();
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
-    }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(id) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.id === id) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-//Event: Add a Book
-document.getElementById("book-form").addEventListener('submit', (e) => {
-    e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-  //Instatiate book
-  const book = new Book(title, author);
+// Event: Add a Book
+document.getElementById('book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const id = Date.now();
 
-  //Add Book to UI
+  // Instatiate book
+  const book = new Book(title, author, id);
+
+  // Add Book to UI
   UI.addBookToList(book);
 
-  //Add book to store
+  // Add book to store
   Store.addBook(book);
 
-  //Clear fields
+  // Clear fields
   UI.clearFields();
 });
 
-//Event: Remove a Book
+// Event: Remove a Book from UI
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    //Remove book from UI
-   UI.deleteBook(e.target);
-
-   //Remove book from store
-   
-
+  UI.deleteBook(e.target);
 });
-
-
-
-
